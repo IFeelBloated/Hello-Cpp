a6f0446f9c5d2b399f3a1c64ad856eabfe680a64
Skylar Moore <nickgray0@gmail.com>
2016-06-15 06:00:34 -0700

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -7,15 +7,20 @@ constexpr auto VERSION = (1ull << 63) + 0; //v1.0
 auto main()->void {
 	std::fstream file;
 	char a[][3] = { { 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 } };
-	char b[3][3];
-	memset(b, 0, sizeof(b));
 	file.open("data.dat", std::ios::out);
 	file.write(reinterpret_cast<char *>(a), sizeof(a));
 	file.close();
+	memset(a, 0, sizeof(a));
+	for (auto x : a) {
+		for (auto i = 0; i < 3; ++i)
+			std::cout << static_cast<int>(x[i]) << " ";
+		std::cout << std::endl;
+	}
 	file.open("data.dat", std::ios::in);
-	file.read(reinterpret_cast<char *>(b), sizeof(b));
+	file.read(reinterpret_cast<char *>(a), sizeof(a));
 	file.close();
-	for (auto x : b) {
+	std::cout << std::endl;
+	for (auto x : a) {
 		for (auto i = 0; i < 3; ++i)
 			std::cout << static_cast<int>(x[i]) << " ";
 		std::cout << std::endl;
